07718e1ec17b7a70973f32f617ee7b4698f2f449
TonySwity <goldocean@live.com>
2023-06-21 17:16:26 +0300

    add method collapse

--- Assets/Scripts/MergeController.cs
+++ Assets/Scripts/MergeController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MergeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private float _decelerationFactor = 0.1f;
+    private float _minDistance = 0.2f;
+
+    public void Collapse(ActiveItem fromItem, ActiveItem toItem)
     {
-        
+        if (Mathf.Abs(fromItem.transform.position.y - toItem.transform.position.y) <= _minDistance)
+        {
+            StartCoroutine(MergeProcess(fromItem, toItem));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private IEnumerator MergeProcess(ActiveItem fromItem, ActiveItem toItem)
     {
-        
+        if (fromItem.CurrentItemType == toItem.CurrentItemType)
+        {
+            Vector3 startPosition = fromItem.transform.position;
+
+            for (float t = 0; t < 1f; t += Time.deltaTime / _decelerationFactor)
+            {
+                fromItem.transform.position = Vector3.Lerp(startPosition, toItem.transform.position, t);
+                yield return null;
+            }
+
+            fromItem.transform.position = toItem.transform.position;
+        }
     }
 }
