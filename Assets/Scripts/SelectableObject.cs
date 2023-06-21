2d245dd8c759406bbab14933162c956a5c9b7b1a
TonySwity <goldocean@live.com>
2023-06-22 01:27:50 +0300

    add SelectableObject.cs

---
+++ Assets/Scripts/SelectableObject.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SelectableObject : MonoBehaviour
+{
+    public virtual void OnHover()
+    {
+        transform.localScale = Vector3.one * 1.1f;
+    }
+
+    public virtual void OnUnhover()
+    {
+        transform.localScale = Vector3.one;
+    }
+    
+}
