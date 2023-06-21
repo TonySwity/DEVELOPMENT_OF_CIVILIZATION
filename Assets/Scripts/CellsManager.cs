f6c7cc9b0747d147c210368368645588b7ddcb14
TonySwity <goldocean@live.com>
2023-06-21 17:00:34 +0300

    division MergeSpawnManager -> CellsManager.cs MergeController

--- Assets/Scripts/CellsManager.cs
+++ Assets/Scripts/CellsManager.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MergeSpawnManager : MonoBehaviour
+public class CellsManager : MonoBehaviour
 {
     [field: SerializeField] private List<Cell> _availableCells = new List<Cell>();
 
     private void Start()
     {
         Cell[] cells = FindObjectsOfType<Cell>();
-        
     }
 }
