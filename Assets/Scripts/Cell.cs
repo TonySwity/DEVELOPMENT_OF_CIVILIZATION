34fbdf14c2f5d208acf7e9d2b97d7aa6de42b67a
TonySwity <goldocean@live.com>
2023-06-22 20:38:02 +0300

    add cell metods

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -2,4 +2,17 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
+
+    public bool IsClear { get; private set; } = true;
+
+
+    public void MakeBusy()
+    {
+        IsClear = false;
+    }
+
+    public void MakeClear()
+    {
+        IsClear = true;
+    }
 }
