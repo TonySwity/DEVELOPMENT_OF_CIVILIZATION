36fb79ee26794eab39b624fbfdb75616e7b49418
TonySwity <goldocean@live.com>
2023-06-24 19:05:35 +0300

    rename MergeManager.cs -> MergeSystem.cs

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-
     public bool IsClear { get; private set; } = true;
-
-
+    
     public void MakeBusy()
     {
         IsClear = false;
---
+++ Assets/Scripts/MergeSystem.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class MergeSystem : MonoBehaviour
+{
+    private float _decelerationFactor = 0.1f;
+    private float _mergeTime = 2f;
+
+    public static MergeSystem Instance;
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public void Collapse(ActiveItem firstActiveItem, ActiveItem secondActiveItem)
+    {
+        ActiveItem fromItem;
+        ActiveItem toItem;
+
+        if (firstActiveItem.ItemID > secondActiveItem.ItemID)
+        {
+            fromItem = firstActiveItem;
+            toItem = secondActiveItem;
+        }
+        else
+        {
+            fromItem = secondActiveItem;
+            toItem = firstActiveItem;
+        }
+
+        StartCoroutine(MergeProcess(fromItem, toItem));
+    }
+
+    private IEnumerator MergeProcess(ActiveItem fromItem, ActiveItem toItem)
+    {
+        if (fromItem.CurrentItemType == toItem.CurrentItemType)
+        {
+            Vector3 startPosition = fromItem.transform.position;
+
+            for (float t = 0; t < _mergeTime; t += Time.deltaTime / _decelerationFactor)
+            {
+                fromItem.transform.position = Vector3.Lerp(startPosition, toItem.transform.position, t);
+                yield return null;
+            }
+
+            fromItem.transform.position = toItem.transform.position;
+        }
+    }
+}
