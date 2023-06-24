fe9e1932cb07667e201bf8ee4264e2570a4e213c
TonySwity <goldocean@live.com>
2023-06-24 19:04:27 +0300

    add NextItem, method AddItemID, OnTriggerStay

--- Assets/Scripts/ActiveItem.cs
+++ Assets/Scripts/ActiveItem.cs
@@ -1,9 +1,22 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ActiveItem : Item
 {
+    [field: SerializeField] private ItemType NextItem;
+    [field: SerializeField]public int ItemID { get; private set; }
 
+    public void AddItemID(int itemID)
+    {
+        ItemID = itemID;
+    }
+    
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("stay");
+        if (other.attachedRigidbody.TryGetComponent(out ActiveItem activeItem))
+        {
+            Debug.Log("соединились");
+            MergeSystem.Instance.Collapse(this, activeItem);
+        }
+    }
 }
