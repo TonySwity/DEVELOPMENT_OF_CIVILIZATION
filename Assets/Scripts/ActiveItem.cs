89e96debde86364099a6177b88b9859fa2b54ef0
TonySwity <goldocean@live.com>
2023-06-26 20:27:37 +0300

    change field and add methods ActivateMerge Deactivate

--- Assets/Scripts/ActiveItem.cs
+++ Assets/Scripts/ActiveItem.cs
@@ -1,20 +1,43 @@
+using System;
 using UnityEngine;
 
 public class ActiveItem : Item
 {
-    [field: SerializeField] private ItemType NextItem;
-    [field: SerializeField]public int ItemID { get; private set; }
-
-    public void AddItemID(int itemID)
-    {
-        ItemID = itemID;
-    }
+    //
+    private MergeSystem _mergeSystem;
+    //
+    public ItemType CurrentItemType  => ItemType; 
     
+    [field: SerializeField] private ItemType NextItem;
+    [field: SerializeField] public int ItemID { get; private set; }
+
+    [field: SerializeField] public bool IsActivateMerge { get; private set; }
+
+    //
+    public void Init(MergeSystem mergeSystem) => _mergeSystem = mergeSystem;
+    //
+    
+    public void AddItemID(int itemID) => ItemID = itemID;
+
+    public void ActivatedMerge() => IsActivateMerge = true;
+
+    public void DeactivateMerge() => IsActivateMerge = false;
+
     private void OnTriggerStay(Collider other)
     {
-        if (other.TryGetComponent(out ActiveItem activeItem))
+        
+        if (other.TryGetComponent(out ActiveItem activeItem) == false)
+        {
+            return;
+        }
+        
+        if (IsActivateMerge && activeItem.IsActivateMerge)
         {
             MergeSystem.Instance.Collapse(this, activeItem);
+            
+            //
+            //_mergeSystem.Collapse(this, activeItem);
+            //
         }
     }
 }
