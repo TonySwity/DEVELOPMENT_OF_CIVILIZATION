a2bc46c3f24a6c09326a43d9f8c6daad4fa51d18
TonySwity <goldocean@live.com>
2023-06-26 20:31:35 +0300

    add field and method

--- Assets/Scripts/SelectableObject.cs
+++ Assets/Scripts/SelectableObject.cs
@@ -1,15 +1,30 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(ActiveItem))]
 public class SelectableObject : MonoBehaviour
 {
+    private ActiveItem _activeItem;
+
+    private void Start()
+    {
+        _activeItem = GetComponent<ActiveItem>();
+    }
     public virtual void OnHover()
     {
         transform.localScale = Vector3.one * 1.1f;
+        _activeItem.DeactivateMerge();
     }
 
     public virtual void OnUnhover()
     {
         transform.localScale = Vector3.one;
+        _activeItem.ActivatedMerge();
+    }
+
+    public ItemType GetCurrentItemType()
+    {
+        return _activeItem.CurrentItemType;
     }
     
 }
