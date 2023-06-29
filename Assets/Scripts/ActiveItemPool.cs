72ac4270e7acf6b2ef4af316db960f8339d4ae6a
TonySwity <goldocean@live.com>
2023-06-29 23:28:29 +0300

    add ActiveItemPool

---
+++ Assets/Scripts/ActiveItemPool.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ActiveItemPool : MonoBehaviour
+{
+    [SerializeField] private Transform _container;
+    [SerializeField] private int _capacityOfEachType = 20;
+
+    private List<ActiveItem> _pool = new List<ActiveItem>();
+
+    protected void Initialize(ActiveItem activeItem)
+    {
+        ActiveItem spawned = Instantiate(activeItem, _container);
+        spawned.gameObject.SetActive(false);
+        
+        _pool.Add(spawned);
+    }
+
+    protected bool TryGetActiveItem(ItemType itemType, out ActiveItem activeItemResult)
+    {
+        activeItemResult = _pool.FirstOrDefault(item => item == item.gameObject.activeSelf && item.CurrentItemType == itemType);
+
+        return activeItemResult != null;
+    }
+}
