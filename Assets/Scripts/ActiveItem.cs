f952a391f8d4ed2a9fe9dd66ee735a12efe1a351
TonySwity <goldocean@live.com>
2023-07-05 19:43:51 +0300

    change method delete OnTriggerStay

--- Assets/Scripts/ActiveItem.cs
+++ Assets/Scripts/ActiveItem.cs
@@ -1,20 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ActiveItem : Item
 {
+    [SerializeField] private LayerMask _layerMask;
     private Cell _currentCell;
     private MergeSystem _mergeSystem;
-
+    
     public Cell CurrentCell => _currentCell;
     public ItemType CurrentItemType => ItemType;
     public ItemType NextItem => NextItemType;
     [field: SerializeField] public int ItemID { get; private set; }
     [field: SerializeField] public bool IsActivateMerge { get; private set; }
-    
+
     public void Init(MergeSystem mergeSystem)
-    { 
+    {
         _mergeSystem = mergeSystem;
-    } 
+    }
 
     public void SetCurrentCell(Cell currentCell) => _currentCell = currentCell;
 
@@ -23,20 +25,32 @@ public class ActiveItem : Item
     public void ActivatedMerge() => IsActivateMerge = true;
 
     public void DeactivateMerge() => IsActivateMerge = false;
-    
-    public void ResetItemTypeCell() => _currentCell.SetCurrentItemType(ItemType.Empty);
 
-    private void OnTriggerStay(Collider other)
+    public void ResetItemTypeCell() => _currentCell.SetCurrentItemType(ItemType.Empty);
+    
+    public void FindFirstColliderToMerge()
     {
-        if (other.TryGetComponent(out ActiveItem activeItem))
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 0.7f, _layerMask);
+        
+        int minColliders = 2;
+        int secondCollider = 0;
+
+        if (colliders.Length >= minColliders)
         {
-            if (ItemID < activeItem.ItemID)
+            for (int i = 0; i <colliders.Length-1; i++)
             {
-                if (IsActivateMerge && activeItem.IsActivateMerge)
+                if (colliders[i].TryGetComponent(out ActiveItem activeItem))
                 {
-                    _mergeSystem?.Collapse(this, activeItem);
+                    if (ItemID != activeItem.ItemID && activeItem.ItemID < ItemID)
+                    {
+                        if (IsActivateMerge && activeItem.IsActivateMerge)
+                        {
+                            _mergeSystem?.Collapse(this, activeItem);
+                        }
+                    }
                 }
             }
+
         }
     }
 }
