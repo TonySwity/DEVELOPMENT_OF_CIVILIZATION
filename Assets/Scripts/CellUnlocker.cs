0e68a098d23912815f2e1bc9a27c3b79bf8e0073
TonySwity <goldocean@live.com>
2023-07-08 02:28:59 +0300

    add

--- Assets/Scripts/CellUnlocker.cs
+++ Assets/Scripts/CellUnlocker.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CellsManager : MonoBehaviour
+public class CellUnlocker : MonoBehaviour
 {
     [field: SerializeField] private List<Cell> _availableCells = new List<Cell>();
 
     private void Start()
     {
-        Cell[] cells = FindObjectsOfType<Cell>();
+        Cell[] cells = transform.GetComponentsInChildren<Cell>();
+        _availableCells.AddRange(cells);
     }
 }
