4b9296d9a19cfa7164e7fee15ad7903531c22019
TonySwity <goldocean@live.com>
2023-07-09 01:08:36 +0300

    add event and set c0lor

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,12 +1,22 @@
+using System;
 using UnityEngine;
 
-public class Cell : MonoBehaviour
+public class Cell : MonoBehaviour, ICellable
 {
     [field: SerializeField] public ItemType CurrentItemType { get; private set; }
-
+    [field: SerializeField] public Color CurrentColor { get; private set; }
+    
+    public event Action<ItemType> Achieved;
+    
+    
     public void SetCurrentItemType(ItemType itemType)
     {
         CurrentItemType = itemType;
+        Achieved?.Invoke(CurrentItemType);
+    }
+    public void SetColor(Color color)
+    {
+        CurrentColor = color;
     }
 }
 
