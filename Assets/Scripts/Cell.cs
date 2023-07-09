b067537bb6efe8bd90a08913c443df4afbb18eaa
TonySwity <goldocean@live.com>
2023-07-09 16:42:43 +0300

    realization ICellable

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,22 +1,46 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshRenderer), typeof(SphereCollider))]
 public class Cell : MonoBehaviour, ICellable
 {
     [field: SerializeField] public ItemType CurrentItemType { get; private set; }
-    [field: SerializeField] public Color CurrentColor { get; private set; }
+    
+    private MeshRenderer _meshRenderer;
+    private SphereCollider _sphereCollider;
     
     public event Action<ItemType> Achieved;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _sphereCollider = GetComponent<SphereCollider>();
+    }
     
     
     public void SetCurrentItemType(ItemType itemType)
     {
         CurrentItemType = itemType;
-        Achieved?.Invoke(CurrentItemType);
+        
+        if (CurrentItemType != ItemType.Empty)
+        {
+            Achieved?.Invoke(CurrentItemType);
+        }
     }
-    public void SetColor(Color color)
+
+    public void EnableCollider()
     {
-        CurrentColor = color;
+        _sphereCollider.enabled = true;
+    }
+
+    public void DisableCollider()
+    {
+        _sphereCollider.enabled = false;
+    }
+    
+    public void SetColorMaterial(Material material)
+    {
+        _meshRenderer.material = material;
     }
 }
 
