e62288b77fece07f755a3d583a8ee80aa486abee
TonySwity <goldocean@live.com>
2023-07-13 23:55:47 +0300

    ??? create objetPooll EnemyCreator ???

---
+++ Assets/Scripts/EnemyCreator.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+public class EnemyCreator : ObjectPool
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private float _timer;
+    ////???? нужно сделать волны
+}
---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+public class ObjectPool : MonoBehaviour
+{
+   [SerializeField] protected Transform _container;
+   
+   private List<GameObject> _pool = new List<GameObject>();
+    
+   protected void Initialize(GameObject gameObj)
+   {
+      GameObject spawned = Instantiate(gameObject, _container);
+      spawned.gameObject.SetActive(false);
+      _pool.Add(spawned);
+   }
+
+   protected bool TryGetGameObject(GameObject inputGameObject, out GameObject resultGameObject)
+   {
+      resultGameObject = _pool.FirstOrDefault(a => a.gameObject.activeSelf == false);
+
+      return resultGameObject != null;
+   }
+}
