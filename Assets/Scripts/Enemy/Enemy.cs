6a94877d5dafb234bab3c1773ed55fcc88a2a4ea
TonySwity <goldocean@live.com>
2023-07-16 19:23:01 +0300

    add BaseClass Enemy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,38 +1,8 @@
 using UnityEngine;
-using DG.Tweening;
 
-public class Enemy : MonoBehaviour
+public class Enemy: MonoBehaviour
 {
-    [SerializeField] private Vector3 _endPosition = new Vector3(0f, 0f, -5f);
-    [SerializeField] private float _speed = 7f;
-    
-    [field: SerializeField] public AgeItem AgeItem { get; private set; }
+    public AgeItem AgeItem { get; private set; }
 
-    private void OnEnable()
-    {
-        Move();
-    }
-    
-    private void Move()
-    {
-        Sequence sequence = DOTween.Sequence();
-
-        sequence.Append(transform.DORotate(new Vector3(360, 0, 0), 2f, RotateMode.FastBeyond360).SetRelative(true).SetEase(Ease.Linear).SetLoops(-1));
-        sequence.Join(transform.DOMove(_endPosition, _speed).OnComplete(() =>
-        {
-            sequence.Kill(true);
-            gameObject.SetActive(false);
-        }));
-
-    }
-    
-    private void OnTriggerEnter(Collider other)
-    {
-        print(other);
-        if (other.TryGetComponent(out ActiveItem activeItem))
-        {
-            print(activeItem);
-            activeItem.gameObject.SetActive(false);
-        }
-    }
+    protected virtual void Move() {}
 }
