9a5d89aaaa388b1e52a50bcdac866c5d8eef045c
TonySwity <goldocean@live.com>
2023-07-18 00:19:12 +0300

    change move

--- Assets/Scripts/Enemy/Stone.cs
+++ Assets/Scripts/Enemy/Stone.cs
@@ -6,30 +6,36 @@ public class Stone : Enemy
     [SerializeField] private Vector3 _endPosition = new Vector3(0f, 0f, -5f);
     [SerializeField] private float _speed = 7f;
     
-    private void OnEnable()
+    private void Update()
     {
         Move();
+    }
+
+    public void AddEndPoint(Vector3 endPoint)
+    {
+        _endPosition.x = endPoint.x;
+        _endPosition.y = endPoint.y;
     }
     
     protected override void Move()
     {
-        Sequence sequence = DOTween.Sequence();
+        // Sequence sequence = DOTween.Sequence();
+        //
+        // sequence.Append(transform.DORotate(new Vector3(360, 0, 0), 2f, RotateMode.FastBeyond360).SetRelative(true).SetEase(Ease.Linear).SetLoops(-1));
+        // sequence.Join(transform.DOMove(_endPosition, _speed).OnComplete(() =>
+        // {
+        //     sequence.Kill(true);
+        //     gameObject.SetActive(false);
+        // }));
 
-        sequence.Append(transform.DORotate(new Vector3(360, 0, 0), 2f, RotateMode.FastBeyond360).SetRelative(true).SetEase(Ease.Linear).SetLoops(-1));
-        sequence.Join(transform.DOMove(_endPosition, _speed).OnComplete(() =>
-        {
-            sequence.Kill(true);
-            gameObject.SetActive(false);
-        }));
-
+        transform.position += Vector3.back * (_speed * Time.deltaTime);
+        transform.Rotate(Vector3.right * (180f * Time.deltaTime));;
     }
     
     private void OnTriggerEnter(Collider other)
     {
-        print(other);
         if (other.TryGetComponent(out ActiveItem activeItem))
         {
-            print(activeItem);
             activeItem.gameObject.SetActive(false);
         }
     }
