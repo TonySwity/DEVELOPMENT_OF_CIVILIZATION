c7f68ecb5caf12d72bcf753f12338058bc65ba6a
TonySwity <goldocean@live.com>
2023-07-18 00:16:33 +0300

    ref

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Enemy: MonoBehaviour
 {
-    public AgeItem AgeItem { get; private set; }
+    [field: SerializeField]public AgeItem AgeItem { get; private set; }
 
     protected virtual void Move() {}
 }
