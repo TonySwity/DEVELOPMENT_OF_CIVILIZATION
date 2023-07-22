2b54048c8f70b7ed0046a8cb424db1b6e9521c51
TonySwity <goldocean@live.com>
2023-07-22 23:39:48 +0300

    add GameMechanicsCenter.cs

---
+++ Assets/Scripts/GameMechanicsCenter.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+public class GameMechanicsCenter: MonoBehaviour
+{
+    [SerializeField] private ActiveItemSpawner _activeItemSpawner;
+    [SerializeField] private EnemySpawner _enemySpawner;
+    [SerializeField] private AgeGroupsUnlocker _ageGroups;
+}
+
