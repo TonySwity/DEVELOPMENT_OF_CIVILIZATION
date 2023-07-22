13eb21b659e75fbcd668123dbe74fd52839220ca
TonySwity <goldocean@live.com>
2023-07-22 12:17:58 +0300

    delete no use field and method

--- Assets/Scripts/Enemy/Stone.cs
+++ Assets/Scripts/Enemy/Stone.cs
@@ -2,21 +2,13 @@ using UnityEngine;
 
 public class Stone : Enemy
 {
-    [SerializeField] private Vector3 _endPosition = new Vector3(0f, 0f, -5f);
     [SerializeField] private float _speed = 7f;
     
     private void Update()
     {
         Move();
     }
-
-    public void AddEndPoint(Vector3 endPoint)
-    {
-        _endPosition.x = endPoint.x;
-        _endPosition.y = endPoint.y;
-    }
     
-
     protected override void Move()
     {
         transform.position += Vector3.back * (_speed * Time.deltaTime);
