c6efb7e75af417db773362ea586a0765de7cef71
TonySwity <goldocean@live.com>
2023-07-27 18:56:17 +0300

    add OnMouseDown OnMouseDrag OnMouseUp

--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -4,6 +4,8 @@ public class DragObject: ActiveItem
 {
     private Plane _dragPlane;
     private Camera _camera;
+    private Vector3 _startPosition;
+    [SerializeField] private LayerMask _layerMaskCell;
     
     private void Awake()
     {
@@ -13,7 +15,28 @@ public class DragObject: ActiveItem
 
     private void OnMouseDown()
     {
+        SetStartPosition();
+        
+        OnHover();
+        
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out RaycastHit hit, Constants.DragObject.MaxDistanceRay, _layerMaskCell) == false)
+        {
+            return;
+        }
 
+        if (hit.collider.TryGetComponent(out Cell cell) == false)
+        {
+            return;
+        }
+
+        if (cell.CurrentItemType == ItemType.Empty)
+        {
+            return;
+        }
+        
+        cell.SetCurrentItemType(ItemType.Empty);
     }
  
     private void OnMouseDrag()
@@ -26,6 +49,40 @@ public class DragObject: ActiveItem
 
     private void OnMouseUp()
     {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
         
+        if (Physics.Raycast(ray, out RaycastHit hit, Constants.DragObject.MaxDistanceRay, _layerMaskCell) == false)
+        {
+            SetNewPosition(_startPosition);
+            OnUnhover();
+            return;
+        }
+
+        if (hit.collider.TryGetComponent(out Cell cell) == false)
+        {
+                
+            return;
+        }
+
+        if (cell.CurrentItemType != ItemType.Empty)
+        {
+            SetNewPosition(_startPosition);
+            OnHover();
+            return;
+        }
+        
+        SetNewPosition(cell.transform.position);
+        cell.SetCurrentItemType(CurrentItemType);
+        OnUnhover();
+    }
+    
+    private void SetNewPosition(Vector3 position)
+    {
+        transform.position = new Vector3(position.x, Constants.DragObject.OffsetY, position.z);
+    }
+
+    private void SetStartPosition()
+    {
+        _startPosition = new Vector3(transform.position.x, Constants.DragObject.OffsetY, transform.position.z);
     }
 }
