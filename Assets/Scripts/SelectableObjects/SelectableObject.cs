c995dfd757f2464f91dfba6ced555dc68e86945b
TonySwity <goldocean@live.com>
2023-07-28 23:33:32 +0300

    delete no use code

--- Assets/Scripts/SelectableObjects/SelectableObject.cs
+++ Assets/Scripts/SelectableObjects/SelectableObject.cs
@@ -2,23 +2,13 @@ using UnityEngine;
 
 public class SelectableObject : MonoBehaviour
 {
-    //public ActiveItem ActiveItem { get; private set; }
-
-    //private void Start() => ActiveItem = GetComponent<ActiveItem>();
-
     public virtual void OnHover()
     {
         transform.localScale = Vector3.one * 1.1f;
-        //ActiveItem.DeactivateMerge();
     }
 
     public virtual void OnUnhover()
     {
         transform.localScale = Vector3.one;
-        //ActiveItem.ActivatedMerge();
-        //ActiveItem.FindFirstColliderToMerge();
     }
-
-    //public ItemType GetCurrentItemType() => ActiveItem.CurrentItemType;
-
 }
