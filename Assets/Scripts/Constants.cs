957a584d97037b732b79a7d34a009a3c02f966a9
TonySwity <goldocean@live.com>
2023-08-01 12:20:46 +0300

    update

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -10,7 +10,7 @@ public static class Constants
     public static class ActiveItemMerge
     {
         public const float RadiusSphere = 0.9f;
-        public const float OffsetY = 1f;
+        public const float OffsetY = 2f;
     }
 }
 
