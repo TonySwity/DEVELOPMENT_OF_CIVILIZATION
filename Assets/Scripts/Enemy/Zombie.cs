bf78b51ceea4d6dbd5e962fa587c476e736351d9
TonySwity <goldocean@live.com>
2023-08-01 22:19:43 +0300

    delete magic number

--- Assets/Scripts/Enemy/Zombie.cs
+++ Assets/Scripts/Enemy/Zombie.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Zombie : Enemy
 {
     [SerializeField] private float _speed = 7f;
-    
+    [SerializeField] private float _rotateSpeed = 180f;
     private void Update()
     {
         Move();
@@ -12,6 +12,6 @@ public class Zombie : Enemy
     protected override void Move()
     {
         transform.position += Vector3.back * (_speed * Time.deltaTime);
-        transform.Rotate(Vector3.right * (180f * Time.deltaTime));;
+        transform.Rotate(Vector3.right * (_rotateSpeed * Time.deltaTime));;
     }
 }
