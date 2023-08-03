558af291097be6e8371a385552bfd01ffcc029cd
TonySwity <goldocean@live.com>
2023-08-04 01:22:15 +0300

    add activate enemy

--- Assets/Scripts/Cells/AgeGroupsUnlocker.cs
+++ Assets/Scripts/Cells/AgeGroupsUnlocker.cs
@@ -4,11 +4,14 @@ public class AgeGroupsUnlocker : MonoBehaviour
 {
     [SerializeField] private AgeCellsUnlocker[] _ages = {};
     [SerializeField] private Wallet _wallet;
+    [SerializeField] private EnemySpawner _enemySpawner;
 
     private ICellable[] _cells = {};
 
     private void Start()
     {
+        _enemySpawner.DisActivateZombieAttack();
+        _enemySpawner.DisActivateDragonAttack();
         _ages = transform.GetComponentsInChildren<AgeCellsUnlocker>();
         _cells = transform.GetComponentsInChildren<ICellable>();
 
@@ -36,17 +39,18 @@ public class AgeGroupsUnlocker : MonoBehaviour
                 break;
             case ItemType.SpiderMan when _ages[(int)AgeItem.Classic].IsBlock:
                 _ages[(int)AgeItem.Classic].Unlock();
+                _enemySpawner.ActivateZombiesAttack();
                 break;
             case ItemType.Neo when _ages[(int)AgeItem.Modern].IsBlock:
                 _ages[(int)AgeItem.Modern].Unlock();
+                _enemySpawner.DisActivateZombieAttack();
+                _enemySpawner.ActivateDragonAttack();
                 break;
             case ItemType.Grogu when _ages[(int)AgeItem.Future].IsBlock:
                 _ages[(int)AgeItem.Future].Unlock();
+                _enemySpawner.ActivateZombiesAttack();
+                _enemySpawner.ActivateDragonAttack();
                 break;
-            default:
-                print("Произошла незапланированная фигня");
-                break;
-                
         }
     }
 
