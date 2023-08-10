8e3194b86e64729b0c6cc73b734990351cf4d7c2
TonySwity <goldocean@live.com>
2023-08-11 02:52:18 +0300

    add PausePanel

---
+++ Assets/Scripts/UI/PausePanel.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PausePanel : MonoBehaviour
+{
+   [SerializeField] private AudioMixerGroup _mixer;
+
+   private void OnEnable()
+   {
+      Time.timeScale = 0f;
+   }
+
+   private void OnDisable()
+   {
+      Time.timeScale = 1f;
+   }
+
+   public void ToggleMusic(bool enable)
+   {
+      if (enable)
+      {
+         _mixer.audioMixer.SetFloat(Constants.AudioMixer.MusicVolume, Constants.AudioMixer.MaxVolumeValue);
+      }
+      else
+      {
+         _mixer.audioMixer.SetFloat(Constants.AudioMixer.MusicVolume, Constants.AudioMixer.MinVolumeValue);
+      }
+      
+   }
+
+   public void ChangeVolume(float volume)
+   {
+      _mixer.audioMixer.SetFloat(Constants.AudioMixer.MasterVolume, Mathf.Lerp(Constants.AudioMixer.MinVolumeValue, Constants.AudioMixer.MaxVolumeValue, volume));
+   }
+}
