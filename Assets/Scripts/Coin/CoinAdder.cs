b49a72d13e43664e2163397c84dfcd20414ba15c
TonySwity <goldocean@live.com>
2023-08-17 01:36:25 +0300

    add CoinAdder

---
+++ Assets/Scripts/Coin/CoinAdder.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class CoinAdder : ObjectPool
+{
+    [SerializeField] private GameObject _coinPref;
+
+    private Wallet _wallet;
+    private Camera _camera;
+
+    public event Action Finished;
+
+    public void Initialize(Wallet wallet)
+    {
+        _camera = Camera.main;
+        _wallet = wallet;
+        Finished += _wallet.AddMoney;
+
+        for (int j = 0; j < Constants.CoinAdder.CoinCountForPool; j++)
+        {
+            Initialize(_coinPref);
+        }
+    }
+
+    public void StartCoinToWallet(Vector3 positionCell)
+    {
+        StartCoroutine(AddScoreAnimation(positionCell, _wallet.transform.position));
+    }
+
+    private IEnumerator AddScoreAnimation(Vector3 cellPosition, Vector3 walletPosition)
+    {
+        if (TryGetObjectFromPool(out GameObject result))
+        {
+            Vector3 point0 = cellPosition;
+            Vector3 point1 = point0 + Vector3.left * 2.5f + Vector3.back * 3f;
+            Vector3 screenPositionWallet = new Vector3(walletPosition.x + Constants.CoinAdder.AdjustmentX, walletPosition.y + Constants.CoinAdder.AdjustmentY, -_camera.transform.position.z);
+            Vector3 point3 = _camera.ScreenToWorldPoint(screenPositionWallet);
+            Vector3 point2 = point3 + Vector3.back * 4f;
+            result.transform.position = point0;
+            result.SetActive(true);
+
+            for (float time = 0; time < 1f; time += Time.deltaTime)
+            {
+                result.transform.position = Bezier.GetPoint(point0, point1, point2, point3, time);
+                yield return null;
+            }
+
+            result.SetActive(false);
+            Finished?.Invoke();
+        }
+    }
+
+    private void OnDisable()
+    {
+        Finished -= _wallet.AddMoney;
+    }
+}
